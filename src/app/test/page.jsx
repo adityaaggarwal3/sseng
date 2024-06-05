c1be9013cc4df2ee9365d703a8e501c00ec09db9
Aditya <aditya.aggarwal@live.com>
2024-06-06 05:20:00 +0530

    update2  commit

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -4,14 +4,16 @@ import React from "react";
 import styles from "./test.module.css";
 import Image from "next/image";
 import Header from "../components/header/Header";
+import TypingText from "../components/typingText/TypingText";
 
 const test = () => {
   return (
-    <div>
+    <div className={styles.test}>
       <Header />
-      <div className={styles.aboutComp}>
+      <TypingText />
+      {/* <div className={styles.aboutComp}>
         <h2>About Us</h2>
-      </div>
+      </div> */}
     </div>
   );
 };
